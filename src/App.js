99a2b40bd32d8f263565c5baddf815e02d6aef04
Park-SC-dev <ajt585@gmail.com>
2025-03-28 00:47:52 +0900

    basename 추가

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
 function App() {
-  return <Router>
+  return <Router basename={process.env.PUBLIC_URL}>
     <Routes>
       <Route path="/movie/:id" element={<Detail />}></Route>
       <Route path="/" element={<Home />}></Route>
